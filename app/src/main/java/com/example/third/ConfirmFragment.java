dbe3c4feb9fd779902fc67feb0a84591f63b2d59
inade <inadezda2003@gmail.com>
2023-03-04 22:51:22 +0300

    внесение корректировок и ясностей

--- app/src/main/java/com/example/third/ConfirmFragment.java
+++ app/src/main/java/com/example/third/ConfirmFragment.java
@@ -6,23 +6,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-
-import com.example.third.databinding.FragmentConfirmBinding;
 import com.example.third.databinding.FragmentConfirmBinding;
 
 public class ConfirmFragment extends Fragment {
     private static String TAG = "tag";
+    private static String TAG1 = "resultText";
     FragmentManager fragmentManager;
     FragmentConfirmBinding binding;
-    com.example.third.MainActivity mainActivity;
+    MainActivity mainActivity;
     public ConfirmFragment(){
         super(R.layout.fragment_confirm);
     }
@@ -30,7 +26,7 @@ public class ConfirmFragment extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        mainActivity = (com.example.third.MainActivity) context;
+        mainActivity = (MainActivity) context;
         Log.d(TAG, "onAttach");
         Toast toast = Toast.makeText(getContext(), "onAttach", Toast.LENGTH_LONG );
         toast.show();
@@ -44,10 +40,10 @@ public class ConfirmFragment extends Fragment {
         Toast toast = Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG );
         toast.show();
         Log.d(TAG, "onCreate");
-        // Принятие имени
 
-        fragmentManager.setFragmentResultListener("change to RepositoryFragment", this, (requestKey, result) -> {
-            String results = result.getString("resultText");
+        // Мы получаем наше имя из другого фрагмента
+        fragmentManager.setFragmentResultListener("ppp", this, (requestKey, result) -> {
+            String results = result.getString(TAG1);
             binding.textView5ForName.setText("Ваше имя: " + results);
         });
 
@@ -67,77 +63,16 @@ public class ConfirmFragment extends Fragment {
         Log.d(TAG, "onViewCreated");
         Toast toast = Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG );
         toast.show();
-        // Передача имени обратно
-        binding.buttonGotovo1.setOnClickListener(view1 -> {
-            Bundle result = new Bundle();
-            result.putString("resultText", binding.textView5ForName.getText().toString());
-            fragmentManager.setFragmentResult("change to HubFragment", result);
-            mainActivity.changeFragment(view1);
+        // Мы передаем имя обратно в первый фрагмент
+        binding.buttonGotovo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                Bundle result = new Bundle();
+                result.putString(TAG1, binding.textView5ForName.getText().toString());
+                fragmentManager.setFragmentResult("ccc", result);
+                mainActivity.changeFragment(view1);
+            }
         });
     }
 
-    @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
-        Log.d(TAG, "onViewStateRestored");
-        Toast toast = Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG );
-        toast.show();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart");
-        Toast toast = Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG );
-        toast.show();
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume");
-        Toast toast = Toast.makeText(getContext(), "onResume", Toast.LENGTH_LONG );
-        toast.show();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause");
-        Toast toast = Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG );
-        toast.show();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop");
-        Toast toast = Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG );
-        toast.show();
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.d(TAG, "onSaveInstanceState");
-        Toast toast = Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG );
-        toast.show();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Log.d(TAG, "onDestroyView");
-        Toast toast = Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG );
-        toast.show();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy");
-        Toast toast = Toast.makeText(getContext(), "onDestroy", Toast.LENGTH_LONG );
-        toast.show();
-    }
 }
--- app/src/main/java/com/example/third/HelloFragment.java
+++ app/src/main/java/com/example/third/HelloFragment.java
@@ -5,21 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-
 import com.example.third.databinding.FragmentHelloBinding;
 
 public class HelloFragment extends Fragment {
     FragmentManager fragmentManager;
     FragmentHelloBinding binding;
-    com.example.third.MainActivity mainActivity;
+    MainActivity mainActivity;
+    private final String TAG = "resultText";
 
     public HelloFragment(){
         super(R.layout.fragment_hello);
@@ -28,16 +24,16 @@ public class HelloFragment extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        mainActivity = (com.example.third.MainActivity) context;
+        mainActivity = (MainActivity) context;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fragmentManager = getParentFragmentManager();
-        // Ваше имя на главном экране
-        fragmentManager.setFragmentResultListener("change to HubFragment", this, (requestKey, result) -> {
-            String results = result.getString("resultText");
+        // Мы получаем данные с именем обратно
+        fragmentManager.setFragmentResultListener("ccc", this, (requestKey, result) -> {
+            String results = result.getString(TAG);
             binding.textView.setText(results);
         });
     }
@@ -52,11 +48,11 @@ public class HelloFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Передача имени
+        // Мы ввели наше имя и передаем текст
         binding.buttonGotovo.setOnClickListener(view1 -> {
             Bundle result = new Bundle();
-            result.putString("resultText", binding.editTextTextPersonName.getText().toString());
-            fragmentManager.setFragmentResult("change to RepositoryFragment", result);
+            result.putString(TAG, binding.editTextTextPersonName.getText().toString());
+            fragmentManager.setFragmentResult("ppp", result);
             mainActivity.changeFragment(view1);
         });
     }
